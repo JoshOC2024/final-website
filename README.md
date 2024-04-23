abee246d55b657e92f0124dfd3ac1a93d09c2108
Joshua Kruger <JoshuaKruger@olympic.student.edu>
2024-04-22 18:17:35 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-website
+# final-website
---
+++ js/script.js
@@ -0,0 +1,38 @@
+//The workings for the responsive navigation bar at the top. Grabs button names from index and listens to see if the hamburger menu is clicked only when screen is too small.
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+//Shows slide number in top left of home page
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// These are for setting up the slide information on the home page like setting the number of slides based on how many are there 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
